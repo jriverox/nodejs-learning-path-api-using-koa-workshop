667e98032f3ab8680c325ef0ff6d4fda996a1dd2
Jhony Rivero <jhony.rivero@hotmail.com>
2022-03-19 11:26:44 -0500

    fix documentacion

--- src/app.js
+++ src/app.js
@@ -2,8 +2,6 @@ const Koa = require('koa');
 const json = require('koa-json');
 const logger = require('koa-logger');
 const bodyParser = require('koa-bodyparser');
-const mongoose = require('mongoose');
-const yenv = require('yenv');
 
 const errorHandler = require('./middleware/error-handler');
 const LogManager = require('./utils/logging/log-manager');
@@ -12,25 +10,13 @@ const logManager = new LogManager();
 
 const routes = require('./routes');
 
-const env = yenv();
 // Inicializar nuestro servidor usando koa (similar a express)
 const app = new Koa();
 // Inicializar los middleware
 app.use(bodyParser()).use(json()).use(logger()).use(errorHandler);
 
-// eslint-disable-next-line array-callback-return
-routes.map((item) => {
+routes.forEach((item) => {
   app.use(item.routes()).use(item.allowedMethods());
-});
-
-// eslint-disable-next-line no-unused-vars
-app.on('error', (err, ctx) => {
-  // eslint-disable-next-line no-console
-  console.error('logging error');
-  const isOperationalError = logManager.error(err);
-  if (!isOperationalError) {
-    process.exit(1);
-  }
 });
 
 module.exports = app;
