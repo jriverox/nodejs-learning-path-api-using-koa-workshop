0a501b6ef81ef096147b2261c9294ab3723381aa
Jhony Rivero <jhony.rivero@hotmail.com>
2022-03-19 17:49:08 -0500

    episodio 3 codigo

--- src/routes/contacts.route.js
+++ src/routes/contacts.route.js
@@ -7,19 +7,21 @@ const {
   createContact,
 } = require("../controllers/contacts.controller");
 
-// codigo agregado en este paso. Importamos la funcion que hará de middleware
 const { verifyToken } = require('../middleware/auth');
+// codigo agregado en este paso. Importamos la funcion que hará de middleware para validar los esquemas
+const validateSchema = require('../middleware/schema-validator');
+// codigo agregado en este paso. Importamos los 2 esquemas que hemos creados para los 2 endpoints de contacts
+const { byIndexSchema, postSchema } = require('../schemas/contacts.schema');
+// codigo agregado en este paso: creamos una instancia del validador pasandole la parte del request que queremos validar en este caso (params) y el esquema apropiado. Recuerda que depende de la necesidad eso que aqui llamamos la parte de con contexto es en realidad donde vienen los datos en el request, por ejemplo: params, query (query string), body, header.
+const byIndexValidator = validateSchema({ params: byIndexSchema });
+// codigo agregado en este paso: creamos una instancia del validador pasandole la parte del request que queremos validar en este caso (body) y el esquema apropiado
+const postValidator = validateSchema({ body: postSchema });
 
-// codigo agregado en este paso: colocamos lafuncion verifyToken para que se ejecute en cadarequest antes de invocar a la funcion del controllador
 // GET /contacts/29
-router.get("/byIndex", "/:index", verifyToken, getContactByIndex);
-
-// codigo agregado en este paso: colocamos lafuncion verifyToken para que se ejecute en cadarequest antes de invocar a la funcion del controllador
+router.get("/byIndex", "/:index", verifyToken, byIndexValidator, getContactByIndex);
 // POST /contacts/
-router.post("/post", "/", verifyToken, createContact);
-
-// codigo agregado en este paso: colocamos lafuncion verifyToken para que se ejecute en cadarequest antes de invocar a la funcion del controllador
+router.post("/post", "/", verifyToken, postValidator, createContact);
 // PUT /contacts/29
-router.put("/put", "/:index", verifyToken, updateContact);
+router.put("/put", "/:index", verifyToken, byIndexValidator, postValidator, updateContact);
 
 module.exports = router;
